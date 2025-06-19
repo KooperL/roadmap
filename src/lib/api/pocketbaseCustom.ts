ed4014c4873232ba3d51d0d8ab16c4a9dc4a843d
KooperL <kooper.lingohr@gmail.com>
2025-06-19 21:07:00 +1000

    Pocketbase

---
+++ src/lib/api/pocketbaseCustom.ts
@@ -0,0 +1,59 @@
+import axios, { Axios, type AxiosResponse } from 'axios';
+import { currentUser } from '$lib/pocketbase';
+import { env } from '$env/dynamic/public';
+
+const HOST = 'https://kroadmapapp.pockethost.io/' ?? env.PUBLIC_POCKETBASE_URL;
+
+export const pocketbaseCustomEndpoints = {
+} as const;
+
+interface ApiData {
+	url?: [
+		{
+			key: string;
+			value: string;
+		}
+	];
+	payload?: any;
+	method: string;
+}
+
+export async function fetchCustomEndpoint(
+	endpoint: string,
+	data: ApiData
+): Promise<AxiosResponse<any>> {
+	try {
+		let token;
+		currentUser.subscribe((value) => {
+			token = value?.token;
+		});
+
+		let path = endpoint.toString();
+		if (data.url) {
+			for (let item of data.url) {
+				path = path.replace(':' + item.key, item.value);
+			}
+		}
+		if (data.method === 'get' && data.payload) {
+			let queryParams = new URLSearchParams();
+			for (let key in data.payload) {
+				queryParams.set(key, data.payload[key]);
+			}
+			path += '?' + queryParams.toString();
+		}
+		const res = await axios({
+			method: data.method ?? 'post',
+			url: path,
+			headers: {
+				Authorization: token
+			},
+			data: data.method === 'post' ? data.payload : undefined
+		});
+		return res;
+	} catch (e) {
+		console.error(e);
+		return e;
+	}
+}
+
+export default fetchCustomEndpoint;
